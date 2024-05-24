c3f2e53e76daae82f5723021a95f40460a36fefc
Farhad Dubey <95957352+farhaddubey@users.noreply.github.com>
2024-05-24 11:47:00 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI= "mongodb+srv://tdil_consultancy_services:tdilcs@cluster0.bklz6z2.mongodb.net/test";
+const mongoURI= "";
 const connecttoMongo=()=>{
     mongoose.connect(mongoURI,()=>{
         console.log("Connected to Mongo Successfully.");//We could also use async await, while async always returns a promise, await stops run of function until the current function is resolved
